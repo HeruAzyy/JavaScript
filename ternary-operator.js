443f315d68f6674715bde39bd52af27466fcc660
Heru Azy <heruazyp@gmail.com>
2023-11-16 23:04:54 +0700

    Add Materi: ternary operator

---
+++ ternary-operator.js
@@ -0,0 +1,15 @@
+// if Statement
+let nilai = 75
+
+if (nilai >= 75) {
+  console.log('Selamat anda lulus')
+} else {
+  console.log('Maaf anda belum lulus')
+}
+
+// ternary Statement
+// ? saat kondisi terpenuhi (sebagai if) - true
+// : saat kondisi tidak terpenuhi (sebagai else) - false
+let score = 80
+
+score >= 75 ? console.log('Lulus') : console.log('Tidak Lulus')
