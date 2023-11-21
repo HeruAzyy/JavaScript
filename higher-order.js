691b8df334d457165cbac2d38f3e72a6214b48c7
Heru Azy <heruazyp@gmail.com>
2023-11-21 22:06:10 +0700

    Add Materi: Higher Order Func(filter, map, reduce)

--- higher-order.js
+++ higher-order.js
@@ -1,22 +1,54 @@
 // Higher Order Function
 // -> Function yang beroperasi pada function lain. (sebagai argument atau return value)
 // ** Function sebagai argument di sebut CALLBACK
-function tugas(matkul, selesai) {
-  console.log(`Sedang mengerjakan ${matkul}`)
-  selesai()
-}
+// function tugas(matkul, selesai) {
+//   console.log(`Sedang mengerjakan ${matkul}`)
+//   selesai()
+// }
 
-function selesai() {
-  console.log(`Selesai mengerjakan`)
-}
-tugas('Higher Order function', selesai)
+// function selesai() {
+//   console.log(`Selesai mengerjakan`)
+// }
+// tugas('Higher Order function', selesai)
 
 // Contoh lain higher order function
-function salam(waktu) {
-  return function (nama) {
-    console.log(`Halo ${nama}, selamat ${waktu} :)`)
-  }
-}
+// function salam(waktu) {
+//   return function (nama) {
+//     console.log(`Halo ${nama}, selamat ${waktu} :)`)
+//   }
+// }
 
-let malam = salam('Pagi')
-console.log(malam('Heru Azy'))
+// let malam = salam('Pagi')
+// console.log(malam('Heru Azy'))
+
+const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9]
+// ===================================== FILTER
+// mencari angka yang >= 3
+const filter = angka.filter(function (a) {
+  return a >= 3
+})
+console.log(`Hasil filter: ${filter}`)
+
+// ===================================== MAP
+// kalikan semua angka dengan 2
+const map = angka.map(function (a) {
+  return a * 2
+})
+console.log(`Hasil map: ${map}`)
+
+// ===================================== REDUCE
+// jumlahkan semua angka
+// REDUCE MEMPUNYAI 2 ARGUMENT
+const newAngka = angka.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+)
+console.log(`Hasil reduce: ${newAngka}`)
+
+// ===================================== Caining
+const hasil = angka
+  .filter((a) => a > 5)
+  .map((a) => a * 3)
+  .reduce((acc, cur) => acc + cur)
+
+console.log(`Hasil caining method: ${hasil}`)
