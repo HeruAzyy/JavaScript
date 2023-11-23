be6e8ddf00af5f654f333ef095c2b75d2ef0575a
Heru Azy <heruazyp@gmail.com>
2023-11-24 00:04:18 +0700

    Add: Materi tipe data

---
+++ public/data-type.js
@@ -0,0 +1,12 @@
+const nama = 'Heru Azy'; //string
+const umur = 22; //number
+const kerja = true; //boolean
+let arr = []; //object
+
+// Running code ini untuk mengetahui masing-masing tipe data diatas
+console.log(`Tipe data nama adalah: ${typeof nama}`);
+console.log(`Tipe data umur adalah: ${typeof umur}`);
+console.log(`Tipe data kerja adalah: ${typeof kerja}`);
+console.log(`Tipe data arr adalah: ${typeof arr}`);
+
+console.log(`Halo, saya ${nama} dan saya umur ${umur} tahun.`);
